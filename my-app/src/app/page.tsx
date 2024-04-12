de4b4afc79515d08f9884cb1df188c84db0cfb7e
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-12 22:23:24 +0600

    Adding images in home page

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
+import homeImg from "public/home.jpg";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="Car factory"
+          fill
+          style={{ objectFit: "cover" }}
+        ></Image>
+      </div>
+    </div>
+  );
 }
