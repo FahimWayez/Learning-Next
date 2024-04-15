9ef93b2e345e2650a371bc347dd608495538a6f1
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-15 18:07:45 +0600

    Fetching particular records

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -1,4 +1,19 @@
-export default function SnippetShowPage(props: any) {
-  console.log(props);
-  return <div>Show a snippet</div>;
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
+interface SnippetShowPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  const snippet = await db.snippet.findFirst({
+    where: { id: parseInt(props.params.id) }, //parseInt ansi karon amader parameter e ashtese integer thik e, but amader param kintu id ke string hishebe dhore nitese.
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+  return <div>{snippet.title}</div>;
 }
