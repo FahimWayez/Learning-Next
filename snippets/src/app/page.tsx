7c3eb3ff8b588c880e84cc845778d59b7bd21c29
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-15 19:06:17 +0600

    home page modified with some links and ui design

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,12 +1,32 @@
 import Image from "next/image";
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany(); //Ekhane amra directly database theke access nitesi. Jodi bairer kono third party api lagto taile http request kora lagto.
 
   const renderedSnippets = snippets.map((snippet) => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between  items-center p-2 border rounded"
+      >
+        <div>{snippet.title} </div>
+        <div>View</div>
+      </Link>
+    );
   });
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl fond-bold">Snippets</h1>
+        <Link href={"/snippets/new"} className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
