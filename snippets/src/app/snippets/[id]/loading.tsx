2e788556c0ada805931a40134743a9552d5c3698
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-15 18:43:11 +0600

    automatic loading page created

---
+++ snippets/src/app/snippets/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function SnippetLoadingPage() {
+  return <div>Loading</div>; //eta ekhon kaj korbena karon amader database locally e ase. Jodi amra ekta timeout function diye dei je database theke data ashte kisu shomoy lagbe taile loading ta dekha jaite pare. Even baire theke data newar shomoy to kisu time lagbei tokhono to dekha jabei.
+}
