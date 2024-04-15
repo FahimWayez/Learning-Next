99440679a51eb92e50bd41dd5cc1a86ee621f017
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-15 22:19:53 +0600

    handling editor changes

--- snippets/src/components/snippet-edit-form.tsx
+++ snippets/src/components/snippet-edit-form.tsx
@@ -2,11 +2,17 @@
 
 import type { Snippet } from "@prisma/client";
 import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormPage {
   snippet: Snippet;
 }
 export default function SnippetEditForm({ snippet }: SnippetEditFormPage) {
+  const [code, setCode] = useState(snippet.code);
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+
   return (
     <div>
       <Editor
@@ -15,6 +21,7 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormPage) {
         language="javascript"
         defaultValue={snippet.code}
         //options={{ minimap: { enabled: false } }} //ekhane aro kisu chaile amra additional design feature dite pari. Eta minimap shorabe.
+        onChange={handleEditorChange}
       ></Editor>
     </div>
   );
