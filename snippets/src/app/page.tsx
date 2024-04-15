09117d6f4003fe8e9f6acf1c5cc6443afcb62a2d
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-15 16:35:03 +0600

    fetching data with server components

--- snippets/src/app/page.tsx
+++ snippets/src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import { db } from "@/db";
 
-export default function Home() {
-  return <div>Home page</div>;
+export default async function Home() {
+  const snippets = await db.snippet.findMany(); //Ekhane amra directly database theke access nitesi. Jodi bairer kono third party api lagto taile http request kora lagto.
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+  return <div>{renderedSnippets}</div>;
 }
