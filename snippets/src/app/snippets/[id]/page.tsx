dab2e4c6f1f826667c6f100b977dbb5de95b46f2
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-16 12:06:36 +0600

    deleting a record with a server action

--- snippets/src/app/snippets/[id]/page.tsx
+++ snippets/src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/db";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import * as actions from "@/actions";
 interface SnippetShowPageProps {
   params: {
     id: string;
@@ -16,6 +17,7 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
   return (
     <div>
       <div className="flex m-4 justify-between items-center">
@@ -27,7 +29,9 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
           >
             Edit
           </Link>
-          <button className="p-2 border rounded">Delete</button>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
