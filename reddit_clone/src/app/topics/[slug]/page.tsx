5a4a30d939f83adca2ff085754df1abbdce2bb3d
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-18 12:54:15 +0600

    Routing Structure Created

---
+++ reddit_clone/src/app/topics/[slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function TopicShowPage() {
+  return <div>This is the topics show page</div>;
+}
---
+++ reddit_clone/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -0,0 +1,3 @@
+export default function PostShowPage() {
+  return <div>This is the page where posts will be shown</div>;
+}
