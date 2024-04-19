376e91be65bd09850a5a22435436da176db2dbf0
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-19 19:55:10 +0600

    Form validation with zod library

--- reddit_clone/src/actions/create-topic.ts
+++ reddit_clone/src/actions/create-topic.ts
@@ -1,9 +1,28 @@
 "use server";
 
-export async function createTopic(formData: FormData) {
-  const name = formData.get("name");
-  const description = formData.get("description");
+import z from "zod";
 
-  console.log(name, description);
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces.",
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  // console.log(result.error); //eta error ditese karon typescript chay je age check kori age parsing ta successfull hoise kina
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
+
   //TODO: revalidating the homepage after creating a topic
 }
