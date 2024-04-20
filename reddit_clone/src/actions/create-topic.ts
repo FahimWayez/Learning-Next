2499efcf5d415916baa1203ff66714bfe3e4cd97
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-20 12:28:20 +0600

    Showing loading spinners with useFormStatus

--- reddit_clone/src/actions/create-topic.ts
+++ reddit_clone/src/actions/create-topic.ts
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/db";
 import paths from "@/paths";
 import { revalidatePath } from "next/cache";
+import { resolve } from "path";
 
 const createTopicSchema = z.object({
   name: z
@@ -30,6 +31,8 @@ export async function createTopic(
   formState: CreateTopicFormState,
   formData: FormData
 ): Promise<CreateTopicFormState> {
+  // await new Promise((resolve) => setTimeout(resolve, 2500));
+
   const result = createTopicSchema.safeParse({
     name: formData.get("name"),
     description: formData.get("description"),
