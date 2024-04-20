f607219f1edba9c48ab162b3dccef48be2a49ce6
Fahim Wayez <fahimwayez1@gmail.com>
2024-04-21 01:05:37 +0600

    Redirecting from server action

--- reddit_clone/src/components/search-input.tsx
+++ reddit_clone/src/components/search-input.tsx
@@ -1,10 +1,16 @@
-"user client";
+"use client";
 
 import { Input } from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
+import * as actions from "@/actions";
 
-export default function SearchInput() {
+const SearchInput = () => {
   const searchParams = useSearchParams();
 
-  return <Input defaultValue={searchParams.get("term") || ""} />;
-}
+  return (
+    <form action={actions.search}>
+      <Input name="term" defaultValue={searchParams.get("term") || ""} />
+    </form>
+  );
+};
+export default SearchInput;
